ee22cf6ae9eeb6bbc1b7be6dd0b3c4cd4e56a691
mhoow6 <mhoow6@gmail.com>
2021-03-17 19:12:20 +0900

    float형 데이터 출력

--- atens-c/float.cpp
+++ atens-c/float.cpp
@@ -1,8 +1,8 @@
-//#include <stdio.h>
-//
-//void main() {
-//	float num = 1.2f;
-//	double num2 = 3.4;
-//
-//	printf("%.2f %.3f", num, num2);
-//}
+#include <stdio.h>
+
+void main() {
+	float num = 1.2f;
+	double num2 = 3.4;
+
+	printf("%.2f %.3f", num, num2);
+}
