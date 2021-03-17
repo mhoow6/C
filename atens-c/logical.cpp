79a7802a181ca4a1e46eb90ca8d4f6b1c46f6c72
mhoow6 <mhoow6@gmail.com>
2021-03-17 19:12:48 +0900

    논리연산자 학습

--- atens-c/logical.cpp
+++ atens-c/logical.cpp
@@ -1,21 +1,21 @@
-//#include <stdio.h>
-//
-//void main() {
-//	int x;
-//	int y;
-//
-//	printf("x 좌표값을 입력하세요: ");
-//	scanf_s("%d", &x);
-//
-//	printf("y 좌표값을 입력하세요: ");
-//	scanf_s("%d", &y);
-//
-//	int topx = 0;
-//	int topy = 0;
-//
-//	int bottomx = 10;
-//	int bottomy = 10;
-//
-//	int result = (x >= topx && x <= bottomx) && (y >= topy && y <= bottomy);
-//	printf("x: %d, y: %d, result: %d", x, y, result);
-//}
+#include <stdio.h>
+
+void main() {
+	int x;
+	int y;
+
+	printf("x 좌표값을 입력하세요: ");
+	scanf_s("%d", &x);
+
+	printf("y 좌표값을 입력하세요: ");
+	scanf_s("%d", &y);
+
+	int topx = 0;
+	int topy = 0;
+
+	int bottomx = 10;
+	int bottomy = 10;
+
+	int result = (x >= topx && x <= bottomx) && (y >= topy && y <= bottomy);
+	printf("x: %d, y: %d, result: %d", x, y, result);
+}
